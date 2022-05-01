a292f26415e6ffe90b68cb1c76805ff9f316ade5
Jeremy Monat <jemonat@calalum.org>
2022-05-01 14:08:32 -0400

    Add my subgroup of Mocha automated tests namely, x ** 2 https://javascript.info/testing-mocha

--- test.js
+++ test.js
@@ -1,5 +1,20 @@
 describe("pow", function () {
 
+    describe("raises x to power 2", function () {
+
+        function makeTest(x) {
+            let expected = x * x;
+            it(`${x} in the power 2 is ${expected}`, function () {
+                assert.equal(pow(x, 2), expected);
+            });
+        }
+
+        for (let x = 1; x <= 5; x++) {
+            makeTest(x);
+        }
+
+    });
+
     describe("raises x to power 3", function () {
 
         function makeTest(x) {
