1cde9d42fad2614800e86379d43ae9cd4dd90abb
Jeremy Monat <jemonat@calalum.org>
2022-04-30 23:14:20 -0400

    while loop https://javascript.info/while-for

--- alert.js
+++ alert.js
@@ -1,12 +1,19 @@
 "use strict";
 
+let i = 0;
+
+while (i < 3) {
+    alert(`number ${i}!`);
+    i++;
+}
+
 // for (let i = 0; i < 3; i++) { // shows 0, then 1, then 2
 //     alert(i);
 // }
 
-for (let i = 2; i <= 10; i += 2) {
-    alert(i);
-}
+// for (let i = 2; i <= 10; i += 2) {
+//     alert(i);
+// }
 
 // let i = 1;
 // do {
