89f9fc99fec6102d05bfe05f1a5ab6dda4dd4800
Jeremy Monat <jemonat@calalum.org>
2022-05-01 13:11:11 -0400

    Make if block into switch block https://javascript.info/switch

--- alert.js
+++ alert.js
@@ -1,6 +1,35 @@
 "use strict";
 
-let browser = prompt("Which browser are you using?", "")
+let a = +prompt('a?', '');
+
+// If block...
+if (a == 0) {
+    alert(0);
+}
+if (a == 1) {
+    alert(1);
+}
+
+if (a == 2 || a == 3) {
+    alert('2,3');
+}
+
+// ...transformed into switch case block
+switch (a) {
+    case 0:
+        alert(0);
+        break;
+    case 1:
+        alert(1);
+        break;
+    case 2:
+    case 3:
+        alert("2,3");
+        break;
+
+}
+
+// let browser = prompt("Which browser are you using?", "")
 
 // switch (browser) {
 //     case 'Edge':
@@ -18,16 +47,16 @@ let browser = prompt("Which browser are you using?", "")
 //         alert('We hope that this page looks ok!');
 // }
 
-if (browser == "Edge") {
-    alert("You've got the Edge!");
-} else if (browser == "Chrome"
-    || browser == "Firefox"
-    || browser == "Safari"
-    || browser == "Opera") {
-    alert('Okay we support these browsers too');
-} else {
-    alert('We hope that this page looks ok!');
-}
+// if (browser == "Edge") {
+//     alert("You've got the Edge!");
+// } else if (browser == "Chrome"
+//     || browser == "Firefox"
+//     || browser == "Safari"
+//     || browser == "Opera") {
+//     alert('Okay we support these browsers too');
+// } else {
+//     alert('We hope that this page looks ok!');
+// }
 
 // let n = 10;
 
